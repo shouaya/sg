55692b98eb5a63aead33f5c250c8e4cf4a38fff6
cc.zhuang <cc.zhuang@movacloud.co.jp>
2023-01-04 19:15:50 +0900

    fix file name

--- update.rb
+++ update.rb
@@ -1,11 +1,15 @@
 require "open-uri"
 
 BASE_URL = "https://raw.githubusercontent.com/shouaya/sg/main/"
-files = ['lib.rb', 'main.rb', 'Gemfile']
+files = ['lib.rb', 'main.rb', 'Gemfile', 'steps_demo.xls', 'README.md']
 
 files.each do |file_name|
     open(BASE_URL + file_name) do |file_src|
-        File.open("./" + file_name, "wb") do |file|
+        file_target = file_name
+        if file_name == 'steps_demo.xls' && File.exist?('steps.xls')
+            file_target = 'steps.xls'
+        end
+        File.open("./" + file_target, "wb") do |file|
             file.write(file_src.read)
         end
     end
